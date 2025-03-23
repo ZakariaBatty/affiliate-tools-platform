61d684afe13f3962ad5a29ee7d411472051804f7
ZakariaBatty <zbatty1297@gmail.com>
2025-03-23 18:23:12 +0000

    Update Prisma installation command to include legacy peer dependencies

--- setup-prisma.js
+++ setup-prisma.js
@@ -2,7 +2,7 @@
 import { execSync } from 'child_process';
 
 console.log('Installing Prisma and other dependencies...');
-execSync('npm install prisma @prisma/client next-auth jsonwebtoken bcrypt');
+execSync('npm install prisma @prisma/client next-auth jsonwebtoken bcrypt --legacy-peer-deps');
 execSync('npx prisma init');
 
 console.log('Prisma initialized successfully!');
